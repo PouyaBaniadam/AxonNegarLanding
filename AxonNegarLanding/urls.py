db4bff8a57d65166d401f190d56288d7466ea46b
pouya <p,baniadam.dev@gmail.com>
2025-11-23 19:10:35 +0330

    I don't know. might be working on runflare with this settings.

--- AxonNegarLanding/urls.py
+++ AxonNegarLanding/urls.py
@@ -1,27 +1,13 @@
-"""
-URL configuration for AxonNegarLanding project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.root, name='root')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='root')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
-from AxonNegarLanding import settings
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('root.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # This forces Django to serve media files from your mounted Disk
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
