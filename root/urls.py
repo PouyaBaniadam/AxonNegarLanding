544d1a72472fb4d3edfd5daf410c96302b9623c1
pouya <p,baniadam.dev@gmail.com>
2025-12-14 23:56:39 +0330

    Lower file sizes.

--- root/urls.py
+++ root/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from root import views
 
@@ -15,4 +16,5 @@ urlpatterns = [
     path('download/<str:os_name>/', views.DownloadRelease.as_view(), name='download-release'),
     path('api/search/', views.WeblogAutocompleteView.as_view(), name='search-autocomplete'),
     path('plans', views.PremiumPlans.as_view(), name='plans'),
+    path('payment-bridge', TemplateView.as_view(template_name='root/payment_bridge.html'), name='payment-bridge'),
 ]
